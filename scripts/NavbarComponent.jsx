151473f3a6754f44e1c5997c0dd9eb4e8eae1726
thibautvincent <thibautvincent@icloud.com>
2016-07-25 13:16:04 +0200

    Cleanup html

--- scripts/NavbarComponent.jsx
+++ scripts/NavbarComponent.jsx
@@ -21,30 +21,29 @@ module.exports = React.createClass({
 
         isActivePage : function() {
             console.log(document.location.href);
-            //if ()
         },
 
         render : function(){
             this.isActivePage();
             return (
                 <div className="navbar">
-                <span className="discover-button nav-button" onClick={this.redirectHome}>
-                <i className="lines-icon icon-compass" aria-hidden="true"></i>
-                <p>Ontdek</p>
-                </span>
-                <span className="checkin-button nav-button" onClick={this.redirectAantLezen}>
-                <i className="lines-icon icon-eyeglass" aria-hidden="true"></i>
-                <p>Aan't Lezen</p>
-                </span>
-                <span className="profile-button nav-button" onClick={this.redirectMap}>
-                <i className="lines-icon icon-map kaart-button" aria-hidden="true" ></i>
-                <p>Kaart</p>
-                </span>
-                <span className="profile-button nav-button" onClick={this.redirectProfiel}>
-                <i className="lines-icon icon-user profiel-button" aria-hidden="true"></i>
-                <p>Profiel</p>
-                </span>
+                    <span className="discover-button nav-button" onClick={this.redirectHome}>
+                        <i className="lines-icon icon-compass" aria-hidden="true"></i>
+                        <p>Ontdek</p>
+                    </span>
+                    <span className="checkin-button nav-button" onClick={this.redirectAantLezen}>
+                        <i className="lines-icon icon-eyeglass" aria-hidden="true"></i>
+                        <p>Aan't Lezen</p>
+                    </span>
+                    <span className="profile-button nav-button" onClick={this.redirectMap}>
+                        <i className="lines-icon icon-map kaart-button" aria-hidden="true" ></i>
+                        <p>Kaart</p>
+                    </span>
+                    <span className="profile-button nav-button" onClick={this.redirectProfiel}>
+                        <i className="lines-icon icon-user profiel-button" aria-hidden="true"></i>
+                        <p>Profiel</p>
+                    </span>
                 </div>
-                )
+            )
         }
     })
