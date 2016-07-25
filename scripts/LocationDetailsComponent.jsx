09a1010621c77d081f057dd1c9f8dc5b69ca23ac
thibautvincent <thibautvincent@icloud.com>
2016-07-25 13:27:43 +0200

    Extract LocationDetailsComponent

---
+++ scripts/LocationDetailsComponent.jsx
@@ -0,0 +1,19 @@
+var React = require('react');
+var AdresBlockComponent = require('./AdresBlockComponent.jsx');
+var OpeningsComponent = require('./OpeningsComponent.jsx');
+
+/*
+    Details of a specific location on said location's page
+    <LocationDetails/>
+*/
+module.exports = React.createClass({
+    render : function(){
+        //The state will always contain 1 row
+        return (
+            <div className="detail-infos">
+                <AdresBlockComponent data={this.props.data}/>
+                <OpeningsComponent data={this.props.data}/>
+            </div>
+        )
+    }
+})
