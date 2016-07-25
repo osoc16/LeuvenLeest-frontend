885154d1841bfa1daca34295a74f243e4daaacbe
thibautvincent <thibautvincent@icloud.com>
2016-07-25 13:24:23 +0200

    Extract LeuvenMapComponent

---
+++ scripts/LeuvenMapComponent.jsx
@@ -0,0 +1,13 @@
+/*
+Map
+<LeuvenMap/>
+*/
+var LeuvenMap = React.createClass({
+    render : function(){
+        return (
+            <div className="map-container">
+            <img src="../../css/img/map.svg" className="map"/>
+            </div>
+            )
+    }
+})
