a8738a1ec6e09796867b3366dd29a3a6ad5d205f
thibautvincent <thibautvincent@icloud.com>
2016-07-25 13:17:48 +0200

    Extract AddressBlockComponent

---
+++ scripts/AdresBlockComponent.jsx
@@ -0,0 +1,27 @@
+var React = require('react');
+
+/*
+'Adres' block on location's page
+<AdresBlock/>
+*/
+module.exports = React.createClass({
+    render : function(){
+        return (
+            <div className="adres-block">
+            <div className="adres-text">
+            <h3>{this.props.data === undefined ? "" : this.props.data.name}</h3>
+            <p>
+            {this.props.data === undefined ? "" : this.props.data.address}<br/>
+            3000
+            </p>
+            </div>
+            <div className="route-button">
+            <div className="route-content">
+            <i className="lines-icon icon-cursor"></i>
+            <p>Route beschrijving</p>
+            </div>
+            </div>
+            </div>
+        )
+    }
+})
