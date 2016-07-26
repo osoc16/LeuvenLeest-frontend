2f4728e4ae1333596db97a3606ba528f7680ff3b
thibautvincent <thibautvincent@icloud.com>
2016-07-26 11:44:46 +0200

    Show the recent checkins

--- scripts/RecentComponent.jsx
+++ scripts/RecentComponent.jsx
@@ -14,14 +14,28 @@ var RecentComponent = React.createClass({
     },
 
     componentWillMount : function() {
+        var settings = {
+            'async': true,
+            'crossDomain': true,
+            'url': 'http://95.85.15.210/checkin/recent',
+            'method': 'GET',
+            'headers' : {
+                'Authorization' : 'Bearer ' + localStorage.getItem('oAuth_token')
+            }
+        }
 
+         $.ajax(settings).done(function (response) {
+             this.setState({places : response});
+        }.bind(this));
     },
 
     render : function(){
         return (
-            <div className="recent-act home-row">
+            <div className='recent-act home-row'>
                 <h3>Recent bezocht</h3>
-                <LocationRowComponent />
+                {this.state.places.map(function(place){
+                    return <LocationRowComponent key={place.id} data={place} />
+                })}
             </div>
         )
     }
