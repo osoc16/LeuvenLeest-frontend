fbe22a50547ef9b1441e0436cb3e4db94fabfa6c
thibautvincent <thibautvincent@icloud.com>
2016-07-27 15:29:20 +0200

    Remove "c" + indenting

--- scripts/NavbarComponent.jsx
+++ scripts/NavbarComponent.jsx
@@ -27,56 +27,23 @@ var React = require('react');
             return (
 
              <div className="navbar">
-             <span className={endURL == "" ? "discover-button nav-button highlight" : "discover-button nav-button"} onClick={this.redirectHome}>
-             <i className="lines-icon icon-compass" aria-hidden="true"></i>
-             <p>Ontdek</p>
-             </span>
-             <span className={endURL =="global" ? "checkin-button nav-button highlight" : "checkin-button nav-button" } onClick={this.redirectAantLezen}>
-             <i className="lines-icon icon-eyeglass" aria-hidden="true"></i>
-             <p>Aan't Lezen</p>
-             </span>
-             <span className={endURL == "map" ? "profile-button nav-button highlight" : "profile-button nav-button" } onClick={this.redirectMap}>
-             <i className="lines-icon icon-map kaart-button" aria-hidden="true" ></i>
-             <p>Kaart</p>
-             </span>
-             <span className={endURL == "profiel" ? "profile-button nav-button highlight" : "profile-button nav-button" } c onClick={this.redirectProfiel}>
-             <i className="lines-icon icon-user profiel-button" aria-hidden="true"></i>
-             <p>Profiel</p>
-             </span>
+                 <span className={endURL == "" ? "discover-button nav-button highlight" : "discover-button nav-button"} onClick={this.redirectHome}>
+                    <i className="lines-icon icon-compass" aria-hidden="true"></i>
+                    <p>Ontdek</p>
+                 </span>
+                 <span className={endURL =="global" ? "checkin-button nav-button highlight" : "checkin-button nav-button" } onClick={this.redirectAantLezen}>
+                     <i className="lines-icon icon-eyeglass" aria-hidden="true"></i>
+                     <p>Aan't Lezen</p>
+                 </span>
+                 <span className={endURL == "map" ? "profile-button nav-button highlight" : "profile-button nav-button" } onClick={this.redirectMap}>
+                     <i className="lines-icon icon-map kaart-button" aria-hidden="true" ></i>
+                     <p>Kaart</p>
+                 </span>
+                 <span className={endURL == "profiel" ? "profile-button nav-button highlight" : "profile-button nav-button" } onClick={this.redirectProfiel}>
+                     <i className="lines-icon icon-user profiel-button" aria-hidden="true"></i>
+                     <p>Profiel</p>
+                 </span>
              </div>
-
-
-
-            /*<div className="navbar">
-
-            {endURL == "" ?  
-            <span className="discover-button nav-button highlight" 
-            onClick={this.redirectHome}>
-            <i className="lines-icon icon-compass" aria-hidden="true"></i>
-            <p>Ontdek</p>
-            </span> :<div></div> }
-
-            {endURL == "map" ?  
-            <span className="profile-button nav-button highlight" onClick={this.redirectMap}>
-            <i className="lines-icon icon-map kaart-button" aria-hidden="true" ></i>
-            <p>Kaart</p>
-            </span> :<div></div> }
-
-            {endURL == "global" ?  
-            <span className="checkin-button nav-button highlight" onClick={this.redirectAantLezen}>
-            <i className="lines-icon icon-eyeglass" aria-hidden="true"></i>
-            <p>Aan't Lezen</p>
-            </span> :<div></div> }
-
-
-            {endURL == "profiel" ?  
-            <span className="profile-button nav-button" onClick={this.redirectProfiel}>
-            <i className="lines-icon icon-user profiel-button" aria-hidden="true"></i>
-            <p>Profiel</p>
-            </span>:<div></div> }
-
-            </div>*/
-
             )
         },
         componentWillMount:function(){
