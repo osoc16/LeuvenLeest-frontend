53dbf57dc80bc906e7586ba27c72e77cecec378a
thibautvincent <thibautvincent@icloud.com>
2016-07-27 11:58:02 +0200

    Refactor

--- scripts/DichtbijComponent.jsx
+++ scripts/DichtbijComponent.jsx
@@ -7,13 +7,38 @@ Dichtbij
 */
 var DichtbijComponent = React.createClass({
 
+    getInitialState : function() {
+        return {
+            nearbyPlaces : [],
+        }
+    },
+
+    componentWillMount : function() {
+        navigator.geolocation.getCurrentPosition(function(position){
+                var coordinate =  {
+                    lat : position.coords.latitude,
+                    lon : position.coords.longitude
+                };
+                this.setState({userLocation : coordinate}, function() {
+                var settings = {
+                    'crossDomain': true,
+                    'url': 'http://95.85.15.210/places/'+this.state.userLocation['lat']+'/'+this.state.userLocation['lon'],
+                    'method': 'GET',
+                }
+                 $.ajax(settings).done(function (response, status, xhr) {
+                     this.setState({nearbyPlaces : response});
+                }.bind(this));
+            });
+        }.bind(this));
+    },
+
     render : function(){
         return (
             <div className="dichtbij home-row">
             <h3>Dichtbij</h3>
 
             <div className="location-row">
-                {this.props.places.map(function(object, i) {
+                {this.state.nearbyPlaces.map(function(object, i) {
                     return <LocationRowComponent data={object} key={i} />;
                 })}
             </div>
