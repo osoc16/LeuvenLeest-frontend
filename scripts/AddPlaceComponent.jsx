c2e976aa825ad5acf59ec6ab66399105809a0043
thibautvincent <thibautvincent@icloud.com>
2016-07-27 15:32:22 +0200

    Remove logs + indenting

--- scripts/AddPlaceComponent.jsx
+++ scripts/AddPlaceComponent.jsx
@@ -63,9 +63,6 @@ getCoordinate : function(){
 
 /*Callback function that will be trigger when a location is available */
 handleCoordinate : function(position) {
-    var msg = 'Latitude: ' + position.coords.latitude +
-    ' Longitude: ' + position.coords.longitude;
-    console.log(msg);
 
     var coordinate =  {
         lat : position.coords.latitude,
@@ -76,58 +73,45 @@ handleCoordinate : function(position) {
     return coordinate;
 },
 
-
-
-
-
-
-
 render : function() {
     var pos = this.getCoordinate();
     return (
         <div>
-        <form>
-        <label>
-        Type :
-        </label>
+            <form>
+                <label>
+                    Type :
+                </label>
+                <input id='type' name='suggest' list='suggestions' onChange={this.handleChange} />
+                <datalist id='suggestions'>
+                <option value='College library' />
+                <option value='Coffee shop' />
+                <option value='Park' />
+                <option value='Library' />
+                </datalist>
 
-
-
-        <input id='type' name='suggest' list='suggestions' onChange={this.handleChange} />
-        <datalist id='suggestions'>
-        <option value='College library' />
-        <option value='Coffee shop' />
-        <option value='Park' />
-        <option value='Library' />
-        </datalist>
-
-        <br/>
-        <label>
-        Name :
-        </label>
-        <input name='name' type='text' onChange={this.handleChange} /> <br/>
-        <label>
-        Adress :
-        </label>
-        <input name='address' type='text' value={this.state.address} onChange={this.handleChange} /> <br/>
-        <label>
-        Opening hour :
-        </label>
-        <br/>
-        <label>
-        Tot:
-        </label>
-        <input type='number' name='tot' min='00' max='23' step='1' onChange={this.handleChange}/>
-        <label>
-        van:
-        </label>
-        <input type='number' name ='van' min='00' max='23' step='1' onChange={this.handleChange}/>
-
-
-
-
-        <input type='submit' onClick={this.addPlace} />
-        </form>
+                <br/>
+                <label>
+                    Name :
+                </label>
+                <input name='name' type='text' onChange={this.handleChange} /> <br/>
+                <label>
+                    Adress :
+                </label>
+                <input name='address' type='text' value={this.state.address} onChange={this.handleChange} /> <br/>
+                <label>
+                    Opening hour :
+                </label>
+                <br/>
+                <label>
+                    Tot:
+                </label>
+                <input type='number' name='tot' min='00' max='23' step='1' onChange={this.handleChange}/>
+                <label>
+                    van:
+                </label>
+                <input type='number' name ='van' min='00' max='23' step='1' onChange={this.handleChange}/>
+                <input type='submit' onClick={this.addPlace} />
+            </form>
         </div>
         );
 
@@ -140,7 +124,6 @@ handleChange : function(event) {
     switch(name) {
         case 'suggest':
         var id =  categoryId[value];
-        console.log(id);
         this.setState({categoryId : id});
         break;
 
@@ -182,7 +165,6 @@ getAddressByCoordinate : function(){
     }
      $.ajax(settings)
     .done(function (response, textStatus, xhr) {
-        console.log(response);
         var address;
 
         if(response.address.pedestrian !== undefined)
@@ -205,10 +187,6 @@ getAddressByCoordinate : function(){
         if(response.address.postcode !== undefined)
             address+= ' '+response.address.postcode;
 
-
-        console.log(address);
-
-
         self.setState({address: address});
 
         self.getAddressByName();
@@ -229,12 +207,6 @@ getAddressByName : function(){
     var self= this;
 
     var res = this.state.address.replace(new RegExp(' ', 'g'), '+');
-    console.log('Adress name in getAddressByName');
-    console.log(res);
-    //var res2 =res.replace(new RegExp(' ', 'g'), '+');
-    //var res3 =res.replace(new RegExp(' ', 'g'), '');
-    //console.log(res2);
-
 
     var settings = {
         'crossDomain': true,
@@ -243,8 +215,6 @@ getAddressByName : function(){
     }
 
      $.ajax(settings).done(function (response, textStatus, xhr) {
-        console.log('in getaddrssbyName');
-        console.log(response);
         self.setState({lat : response[0].boundingbox[0], lon : response[0].boundingbox[2]});
         return response;
     })
@@ -264,12 +234,7 @@ addPlace : function(event) {
         tot = '0'+tot;
 
     var openingH = tot +'-'+this.state.van;
-    console.log('Addplace openin2');
-    console.log(openingH);
 
-
-
-    console.log(this.state);
     var settings = {
         'crossDomain': true,
         'url': 'http://95.85.15.210/places/add',
@@ -293,24 +258,12 @@ addPlace : function(event) {
 
     }
 
-    console.log('The state before sending');
-    console.log(this.state);
-    console.log('CatId');
-    console.log(this.state.categoryId);
-    console.log('email');
-    console.log(this.state.email);
-
      $.ajax(settings)
     .done(function (response, textStatus, xhr) {
-        console.log(response);
-
         document.location.href = '/';
     })
     .fail(function(){
         console.log('fail');
     });
 }
-
-
-
 });
