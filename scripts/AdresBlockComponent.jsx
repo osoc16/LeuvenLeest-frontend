7be10b77524044ec6e94facac0cb4006d777d5d1
thibautvincent <thibautvincent@icloud.com>
2016-07-28 10:35:21 +0200

    Change exportname + cleanup

--- scripts/AdresBlockComponent.jsx
+++ scripts/AdresBlockComponent.jsx
@@ -4,23 +4,28 @@ var React = require('react');
 'Adres' block on location's page
 <AdresBlock/>
 */
-module.exports = React.createClass({
+var AdresBlockComponent = React.createClass({
+
+    propTypes : {
+        place : React.PropTypes.object.isRequired,
+    },
+
     render : function(){
         return (
             <div className="adres-block">
-            <div className="adres-text">
-            <h3>{this.props.data === undefined ? "" : this.props.data.name}</h3>
-            <p>
-            {this.props.data === undefined ? "" : this.props.data.address}<br/>            
-            </p>
-            </div>
-            <div className="route-button">
-            <div className="route-content">
-            <i className="lines-icon icon-cursor"></i>
-            <p>Route beschrijving</p>
-            </div>
-            </div>
+                <div className="adres-text">
+                    <h3>{this.props.place.name}</h3>
+                    <p>{this.props.place.address}<br/></p>
+                </div>
+                <div className="route-button">
+                    <div className="route-content">
+                        <i className="lines-icon icon-cursor"></i>
+                        <p>Route beschrijving</p>
+                    </div>
+                </div>
             </div>
         )
     }
 })
+
+export default AdresBlockComponent;
