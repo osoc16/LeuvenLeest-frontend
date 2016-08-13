be734dff17a929300c31e8f1a3eb5a5833dc5643
thibautvincent <thibautvincent@icloud.com>
2016-08-13 18:30:18 +0200

    Create errorcomponent

---
+++ scripts/ErrorComponent.jsx
@@ -0,0 +1,29 @@
+var React = require('react');
+
+var ErrorComponent = React.createClass({
+
+    propTypes : {
+        errorMessage : React.PropTypes.string.isRequired,
+    },
+
+    getInitialState : function() {
+        var errorMessage = this.props.errorMessage ? this.props.errorMessage : 'Er deed zich een onbekende error voor. Probeer het later opnieuw.';
+        return {
+            errorMessage : errorMessage,
+        }
+    },
+
+    render : function() {
+        var style = {
+            'BackgroundColor' : 'red'
+        };
+        return (
+            <div style={style} className='error' >
+                <p>{this.state.errorMessage}</p>
+            </div>
+        );
+    },
+
+});
+
+export default ErrorComponent;
